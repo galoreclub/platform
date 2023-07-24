26d927d191937ca4d0ffc86bc48ec6e5abffc4a8
Himanshu Saxena <himanshu.saxena.india@gmail.com>
2023-07-24 23:35:19 +0100

    Creating user auth system

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 require("dotenv").config()
 const express= require('express')
-
+const User= require('./models/user')
+const bcrypt=require('bcryptjs');
 const app =express();
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hello from auth system</h1>")
 })
 
+app.post("/register", (req,res)=> {
+    const {firstName,lastName,email,password}= res.body;
+
+    if(!(email && password && firstName && lastName)){
+        res.status(400).send('All fields are required')
+    }
+
+    const existingUser = User.findOne({email})
+
+    if(existingUser){
+        res.status(401).send("User already exists")
+    }
+
+    const myEncryptpassword=bcrypt.hash(password ,10);
+
+    const user= User.create({
+        firstName,
+        lastName,
+        email:email.toLowercase(),
+        password:myEncryptpassword,
+    })
+})
+ 
 module.exports=app; 
